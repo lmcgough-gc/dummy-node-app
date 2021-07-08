4d0ddf9081ab84cc30eb472062db2bfa9d0a7380
Lisandro <lisandro78@gmail.com>
2021-07-08 07:57:56 -0300

    Add dummy HTTP server

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const app_port = process.env.APP_PORT || 3000;
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(app_port);
+
+process.stdout.write(`Listening on port ${app_port}\n`);
