fa7b148ab3a2355d87cee570cab8a56f6a5f0f21
Lisandro <lisandro78@gmail.com>
2021-07-16 00:33:29 -0300

    Show timestamp

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end(`Hello World at ${new Date().toISOString()}\n`);
 }).listen(app_port);
 
 process.stdout.write(`Listening on port ${app_port}\n`);
